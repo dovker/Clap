b33ea9fa0d99f8ba08c9c2b21574cb8145672d7c
iHelp <dovker@gmail.com>
2021-09-19 02:00:57 +0300

    Moved to docking branch on imgui

--- Build/Clap/thirdparty/SDL/include/SDL_revision.h
+++ Build/Clap/thirdparty/SDL/include/SDL_revision.h
@@ -1,4 +1,4 @@
-#define SDL_REVISION "https://github.com/dovker/Clap.git@db188c80b39eb7f46bc92f50442849924428fea2"
+#define SDL_REVISION "https://github.com/dovker/Clap.git@84d785ab8c125e487c270450faadb364ec7fb8c2"
 #define SDL_REVISION_NUMBER 0
 
 #ifndef SDL_REVISION
