bc7d41967d606ca76bf159ec4b7135c3b9b4b75d
Dovydas Vaičiukynas <dovker@gmail.com>
2022-05-30 09:08:09 +0300

    Gravity builds

--- Debug/Clap/thirdparty/SDL/include/SDL_revision.h
+++
@@ -1,6 +0,0 @@
-#define SDL_REVISION "https://github.com/dovker/Clap.git@a989a4d971951f8a7cf190b8151b926dbc556a40"
-#define SDL_REVISION_NUMBER 0
-
-#ifndef SDL_REVISION
-#define SDL_REVISION ""
-#endif
